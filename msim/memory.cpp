41934ce1f2764e518ca12cfad43642256b49d0ae
DramaCow <sc14809@my.bristol.ac.uk>
2017-10-03 16:29:24 +0100

    made memory printing even more aesthetic

--- msim/memory.cpp
+++ msim/memory.cpp
@@ -1,5 +1,6 @@
 #include "memory.hpp"
 #include <cstring>
+#include <iomanip>
 
 #define WORD(b0,b1,b2,b3) (b0 | b1 << 8 | b2 << 16 | b3 << 24)
 
@@ -16,9 +17,7 @@ uint32_t Memory::operator[](int a)
   //  bytes[addr+1] << 8 * 1 |
   //  bytes[addr+2] << 8 * 2 |
   //  bytes[addr+3] << 8 * 3;
-
   uint32_t word = WORD(bytes[a+0], bytes[a+1], bytes[a+2], bytes[a+3]);
-
   return word;
 }
 
@@ -31,7 +30,17 @@ std::ostream& operator<<(std::ostream& os, const Memory& mem)
 {
   for (int a = 0; a < mem.capacity; a+=4)
   {
-    os << std::hex << WORD(mem.bytes[a+0], mem.bytes[a+1], mem.bytes[a+2], mem.bytes[a+3]) << ' ';
+    if (a % (4*8) == 0)
+    {
+      os << std::hex << a << '\t';
+    }
+
+    os << std::setfill('0') << std::setw(8) << std::hex << WORD(mem.bytes[a+0], mem.bytes[a+1], mem.bytes[a+2], mem.bytes[a+3]) << ' ';
+
+    if (a % (4*8) == (4*7) && a < mem.capacity - 4)
+    {
+      os << '\n';
+    }
   }
   return os;
 }
