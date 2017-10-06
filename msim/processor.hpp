35776cdcabefa53f7ec870d04b1cb2e420aece95
DramaCow <sc14809@my.bristol.ac.uk>
2017-10-06 20:33:31 +0100

    added between state latches - let's see how this goes...

--- msim/processor.hpp
+++ msim/processor.hpp
@@ -12,6 +12,40 @@ class Processor
     void tick();
 
   private:
+    // between pipeline state latches
+    struct Address // 4
+    {
+      uint32_t pc = 0; // program counter
+    };
+    struct Lat_if_id // 1
+    {
+      uint32_t npc; // next program counter (pc + 4)
+      uint32_t op;  // operation and operand as 32bit word
+    };
+    struct Lat_id_exe // 2
+    {
+      uint32_t npc; // next program counter (propagated)
+      uint32_t opcode;
+      uint32_t a;   // value of register rs
+      uint32_t b;   // value of register rt
+      uint32_t imm; // immediate op value
+      uint32_t rd;  // register id for writeback
+    };
+    struct Lat_exe_mem // 3
+    {
+      uint32_t npc;    // next program counter (propagated)
+      uint32_t opcode; // (propagated)
+      uint32_t t;      // result of execution
+      uint32_t rd;     // register id for writback
+    };
+    struct Lat_mem_wb // 4
+    {
+      uint32_t t;   // result of execution (propagated)
+      uint32_t mem; // data read from memory
+      uint32_t rd;  // register id for writeback (propagated)
+    }; 
+
+  private:
     // pipeline state
     int state;
 
@@ -35,6 +69,12 @@ class Processor
     uint32_t t_latch;    // execute --> writeback (result)
     uint32_t c_latch;    // execute --> execute-bypass (compare latch - | gt | lt | eq |)
 
+    Address     address;
+    Lat_if_id   lat_if_id;
+    Lat_id_exe  lat_id_exe;
+    Lat_exe_mem lat_exe_mem;
+    Lat_mem_wb  lat_mem_wb;
+
     // pipeline stages
     void fetch();
     void decode();
@@ -45,7 +85,6 @@ class Processor
     int cycles;
     int instructions_executed;
 
-  private:
 };
 
 std::ostream& operator<<(std::ostream& os, const Processor& cpu);
