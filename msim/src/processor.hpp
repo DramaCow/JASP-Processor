9d83c6bb95fdeaac03b1cac24dd549823f1b168c
DramaCow <sc14809@my.bristol.ac.uk>
2017-10-08 11:59:12 +0100

    slight code refactor

--- msim/src/processor.hpp
+++ msim/src/processor.hpp
@@ -12,19 +12,18 @@ class Processor
     void tick();
 
   private:
-    // between pipeline state latches
     struct Address
     {
       uint32_t pc = 0; // program counter
     };
     struct Lat_f_d
     {
-      uint32_t npc = 0;  // next program counter (pc + 4)
+      uint32_t npc = 16; // next program counter (pc + 4)
       uint32_t oreg = 0; // operation and operand as 32bit word
     };
     struct Lat_d_e
     {
-      uint32_t npc = 0;    // next program counter (propagated)
+      uint32_t npc = 12;   // next program counter (propagated)
       uint32_t opcode = 0;
       uint32_t a = 0;      // value of register rs
       uint32_t b = 0;      // value of register rt
@@ -33,7 +32,7 @@ class Processor
     };
     struct Lat_e_m
     {
-      uint32_t npc = 0;    // next program counter (propagated)
+      uint32_t npc = 8;    // next program counter (propagated)
       uint32_t opcode = 0; // (propagated)
       uint32_t cmp = 0;    // comparison flag (occurs automatically)
       uint32_t t = 0;      // result of execution
@@ -42,7 +41,7 @@ class Processor
     };
     struct Lat_m_w
     {
-      uint32_t npc = 0;    // next program counter (propagated)
+      uint32_t npc = 4;    // next program counter (propagated)
       uint32_t opcode = 0; // (propagated)
       uint32_t data = 0;   // result of execution (propagated)
       uint32_t rdest = 0;  // register id for writeback (propagated)
@@ -56,15 +55,17 @@ class Processor
     Memory &imem; // instruction memory
     Memory &dmem; // data memory
 
+    // general purpose registers interface
     RegisterFile regfile;
 
+    // between pipeline stage latches
     Address address;
     Lat_f_d lat_f_d;
     Lat_d_e lat_d_e;
     Lat_e_m lat_e_m;
     Lat_m_w lat_m_w;
 
-    // pipeline stages
+    // pipeline stage advancing functions
     void fetch();
     void decode();
     void execute();
@@ -74,7 +75,6 @@ class Processor
     // statistics recorders
     int cycles;
     int instructions_executed;
-
 };
 
 std::ostream& operator<<(std::ostream& os, const Processor& cpu);
