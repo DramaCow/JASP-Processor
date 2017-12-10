88387ad724bf4ed45d911aef20ecb74f0f59139b
DramaCow <sc14809@my.bristol.ac.uk>
2017-12-10 23:13:59 +0000

    changes

--- msim/src/mu.cpp
+++ msim/src/mu.cpp
@@ -8,6 +8,39 @@ MU::MU(DCache &dcache) :
 {
 }
 
+bool MU::isReady()
+{
+  return true;
+}
+
+void MU::dispatch(LSQ::Shelf shelf)
+{
+  this->shelf = shelf;
+
+  if (this->shelf.type == LSQ::Shelf::LOAD)
+  {
+    this->result = dcache[this->shelf.addr];
+    this->duration = 1;
+    this->writeback = true;
+  }
+  else if (this->shelf.type == LSQ::Shelf::STORE)
+  {
+    this->duration = 1;
+    this->writeback = true;
+  }
+  else
+  {
+    this->duration = 0;
+    this->writeback = false;
+  }
+}
+
+// applied to the current state, updates the next state
+void MU::execute(MU& n_mu)
+{
+  n_mu.duration = this->duration-1 > 0 ? this->duration-1 : 0;
+}
+
 MU& MU::operator=(const MU& mu)
 {
   return *this;
--- msim/src/mu.hpp
+++ msim/src/mu.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "lsq.hpp"
 #include "config.hpp"
 #include "cache.hpp"
 
@@ -13,11 +14,20 @@ class MU
   public:
     MU(DCache &dcache);
 
+    bool isReady();
+
+    void dispatch(LSQ::Shelf shelf);
+    void execute(MU &n_mu);
+
     MU& operator=(const MU& mu);
 
     friend std::ostream& operator<<(std::ostream& os, const MU& mu);
 
   private:
+    LSQ::Shelf shelf;
+    int result;
+    int duration = 0;
+    bool writeback = false;
     DCache &dcache;
 };
 std::ostream& operator<<(std::ostream& os, const MU& mu);
